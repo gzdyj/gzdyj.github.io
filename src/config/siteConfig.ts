74a852e4af5dd2fd4b5750edeb8f21c3e504e5a3
郭争 <86736734@qq.com>
2026-08-01 11:34:17 +0800

    config: 追番/番组计划改用个人账号(Bilibili 277050042, Bangumi 1041515)

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -220,7 +220,7 @@ export const siteConfig: SiteConfig = {
 	// bangumi配置
 	bangumi: {
 		// Bangumi用户ID
-		userId: "1143164",
+		userId: "1041515",
 		// 数据模式：static=构建时获取，dynamic=客户端实时获取
 		// static 模式在构建时获取数据并静态渲染，部署后数据不更新
 		// dynamic 模式在浏览器中实时请求 API，始终显示最新数据
@@ -244,7 +244,7 @@ export const siteConfig: SiteConfig = {
 		// Bilibili 配置
 		bilibili: {
 			// 你的 Bilibili 用户 UID
-			uid: "38932988",
+			uid: "277050042",
 		},
 		// TMDB 配置（可选，需要翻墙）
 		// tmdb: {
